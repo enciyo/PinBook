f14a14640d3ba89072707ccd10ee38a3df10ec1a
kilicmustafa <mustafa@dijitalgaraj.com>
2020-08-06 11:39:21 +0300

    Deneme

--- app/src/main/java/com/enciyo/pinbook/utils/FlowExt.kt
+++ app/src/main/java/com/enciyo/pinbook/utils/FlowExt.kt
@@ -1,6 +1,5 @@
 package com.enciyo.pinbook.utils
 
-import com.enciyo.pinbook.utils.livedata.Event
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onEach
 import kotlin.reflect.KFunction1
@@ -11,11 +10,3 @@ fun <T> Flow<T>.onEach(action: KFunction1<T, Unit>): Flow<T> {
     action.invoke(it)
   }
 }
-
-fun <T> Flow<Event<T>>.onEventEach(action: KFunction1<T, Unit>): Flow<Event<T>> {
-  return this.onEach {
-    it.getContentIfNotHandled()?.let(action)
-  }
-}
-
-x
